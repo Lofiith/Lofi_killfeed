3d969e848ad110d50636d909a5debb614c271fd6
Lofiith <148030987+Lofiith@users.noreply.github.com>
2024-12-14 21:41:08 -0600

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,15 +1,49 @@
-RegisterNetEvent('killfeed:display')
-AddEventHandler('killfeed:display', function(killerName, victimName)
+RegisterNetEvent('killfeed:display', function(killerName, victimName)
     SendNUIMessage({
         type = 'updateKillfeed',
-        killer = killerName,
-        victim = victimName
+        killer = killerName or "Unknown",
+        victim = victimName or "Unknown"
     })
 end)
 
-RegisterNetEvent('killfeed:clear')
-AddEventHandler('killfeed:clear', function()
-    SendNUIMessage({
-        type = 'clearKillfeed'
-    })
+RegisterNetEvent('killfeed:clear', function()
+    SendNUIMessage({ type = 'clearKillfeed' })
+end)
+
+-- Track player death state
+CreateThread(function()
+    local isDead = false
+
+    while true do
+        Wait(500)
+
+        local playerPed = PlayerPedId()
+        local currentlyDead = IsEntityDead(playerPed)
+
+        if currentlyDead and not isDead then
+            isDead = true
+
+            local killerPed = GetPedSourceOfDeath(playerPed)
+            local killerId, killerName = -1, "Unknown"
+
+            if killerPed and killerPed ~= playerPed then
+                if IsPedAPlayer(killerPed) then
+                    local killerPlayerId = NetworkGetPlayerIndexFromPed(killerPed)
+                    if killerPlayerId and killerPlayerId ~= -1 then
+                        killerId = GetPlayerServerId(killerPlayerId)
+                        killerName = GetPlayerName(killerPlayerId)
+                    end
+                else
+                    killerName = "Environment"
+                end
+            else
+                killerName = "Self-inflicted"
+            end
+
+            TriggerServerEvent('killfeed:playerDied', killerId)
+
+        elseif not currentlyDead and isDead then
+            isDead = false
+        end
+    end
 end)
